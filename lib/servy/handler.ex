220f072d79b5c3ba6d5dc5007d34132dffaf34f3
Bernardo Corbella <corbellabernardo@gmail.com>
2018-06-20 22:20:02 -0300

    parses request string

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -7,7 +7,13 @@ defmodule Servi.Handler do
   end
 
   def parse(request) do
-    conv = %{method: "GET", path: "/wildthings", resp_body: ""}
+    [method, path, _] =
+      request
+      |> String.split("/n")
+      |> List.first()
+      |> String.split(" ")
+
+    %{method: method, path: path, resp_body: ""}
   end
 
   def route(conv) do
