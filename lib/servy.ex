8e273cd7332483e01a196ef39968e31335c09ae6
Bernardo Corbella <corbellabernardo@gmail.com>
2018-06-20 22:02:02 -0300

    Initial commit + high level transformations

---
+++ lib/servy.ex
@@ -0,0 +1,18 @@
+defmodule Servy do
+  @moduledoc """
+  Documentation for Servy.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Servy.hello
+      :world
+
+  """
+  def hello(name) do
+    "Hello #{name}!"
+  end
+end
---
+++ lib/servy/handler.ex
@@ -0,0 +1,26 @@
+defmodule Servi.Handler do
+  def handle(request) do
+    request
+    |> parse
+    |> route
+    |> format_response
+  end
+
+  def parse(request) do
+    conv = %{method: "GET", path: "/wildthings", resp_body: ""}
+  end
+
+  def route(conv) do
+    conv = %{method: "GET", path: "/wildthings", resp_body: "Bears, Lions, Tigers"}
+  end
+
+  def format_response(conv) do
+    """
+    HTTP/1.1 200 OK
+    Content-Type: text/html
+    Content-Length: 20
+
+    Bears, Lions, Tigers
+    """
+  end
+end
